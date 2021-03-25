2a97e01ecad7d45d5175a996e6b628b0fb471b9f
JavaSelenium88 <56748534+JavaSelenium88@users.noreply.github.com>
2021-03-26 11:39:10 +1300

    Test Aks

---
+++ DemoAutRegister.java
@@ -0,0 +1,12 @@
+package com.k2js.selenium.demoautregister;
+
+public class DemoAutRegister {
+
+	public static void main(String[] abc) {
+		
+		int gen = (int) (Math.random()*8)+1;
+	}
+	sds
+	//Test hkghjgh
+	
+}
